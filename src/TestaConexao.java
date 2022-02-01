cfd3f82d1087cc9ca8c52ae5e89aa996219ba2f8
wslmacieira <wslmacieira@outlook.com>
2022-02-01 11:02:01 -0300

    Primeira conexão

---
+++ src/TestaConexao.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+    public static void main(String[] args) throws SQLException{
+        
+        Connection connection = DriverManager
+            .getConnection("jdbc:mysql://localhost:3307/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+
+            System.out.println("Fechando conexão!");
+        connection.close();
+    }
+}
