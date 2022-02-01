1eda7cee05be90563b5227ec1b76b4707cd5c4fb
wslmacieira <wslmacieira@outlook.com>
2022-02-01 17:26:41 -0300

    Criando pool de conexões

--- src/Connectionfactory.java
+++ src/Connectionfactory.java
@@ -1,12 +1,25 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class Connectionfactory {
 
+    public DataSource dataSource;
+
+    public Connectionfactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3307/loja_virtual?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("root");
+
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection recuperarConexao() throws SQLException {
-        return DriverManager
-                .getConnection("jdbc:mysql://localhost:3307/loja_virtual?useTimezone=true&serverTimezone=UTC", "root",
-                        "root");
+        
+        return this.dataSource.getConnection();
     }
 }
